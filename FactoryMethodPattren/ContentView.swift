0e99404e888e5a17f4b7361b08df9ff131c1275d
va-gog <gohar.vardanyan@picsart.com>
2022-02-11 00:53:26 +0400

    Initial Commit

---
+++ FactoryMethodPattren/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  FactoryMethodPattren
+//
+//  Created by Gohar Vardanyan on 11.02.22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ FactoryMethodPattren/FactoryMethodPattrenApp.swift
@@ -0,0 +1,17 @@
+//
+//  FactoryMethodPattrenApp.swift
+//  FactoryMethodPattren
+//
+//  Created by Gohar Vardanyan on 11.02.22.
+//
+
+import SwiftUI
+
+@main
+struct FactoryMethodPattrenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
